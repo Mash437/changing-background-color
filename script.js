ad9348cb81ac37fd599292869649b0940349437c
Mash437 <mashilo.lekota54@gmail.com>
2025-06-11 10:40:40 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function redBtn(){
+    document.getElementById("background").style.backgroundColor = "red";
+}
+
+function blueBtn(){
+    document.getElementById("background").style.backgroundColor = "blue";
+}
+
+function greenBtn(){
+    document.getElementById("background").style.backgroundColor = "green";
+}
+
+function yellowBtn(){
+    document.getElementById("background").style.backgroundColor = "yellow";
+}
+
+function resetBtn(){
+    document.getElementById("background").style.backgroundColor = "white";
+}
